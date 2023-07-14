e427048b12f261614f80fc7d718cb5f17061c4a7
Vikash Rai <vikash27hbk@gmail.com>
2023-07-14 16:56:04 +0530

    update package.json

--- src/models/db.js
+++ src/models/db.js
@@ -1,16 +1,16 @@
-// const mysql = require('mysql');
-// const dbConfig = require('../config/dbconfig.js');
+const mysql = require('mysql');
+const dbConfig = require('../config/dbconfig.js');
 
-// const connection = mysql.createConnection({
-//   host: dbConfig.HOST,
-//   user: dbConfig.USER,
-//   password: dbConfig.PASSWORD,
-//   database: dbConfig.DB
-// });
+const connection = mysql.createConnection({
+  host: dbConfig.HOST,
+  user: dbConfig.USER,
+  password: dbConfig.PASSWORD,
+  database: dbConfig.DB
+});
 
-// connection.connect(error => {
-//   if (error) throw error;
-//   console.log("Successfully connected to the database.");
-// });
+connection.connect(error => {
+  if (error) throw error;
+  console.log("Successfully connected to the database.");
+});
 
-// module.exports = connection;
+module.exports = connection;
